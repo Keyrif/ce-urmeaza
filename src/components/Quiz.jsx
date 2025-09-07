09f15e1a1b3e5e384a1e8919b90e5b4cc68defcc
KEYRIF <mihaikenj@gmail.com>
2025-09-07 15:09:38 +0300

    hover mobile issue

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -125,7 +125,7 @@ function Quiz ({
                     onClose();
                   }}
                   whileHover={{ scale: 1.05 }}
-                  whileTap={{ scale: 0.95 }}
+                  whileTap={{ scale: 0.9 }}
                   className={`mt-6 px-6 py-3 rounded-full font-semibold shadow-lg duration-300 ${
                     darkMode
                     ? "bg-cyan-700 hover:bg-cyan-600 text-white"
@@ -147,7 +147,7 @@ function Quiz ({
                       <motion.button
                         key={index}
                         onClick={() => quizAnswer(option.scores)}
-                        whileTap={{ scale: 0.97 }}
+                        whileTap={{ scale: 0.95 }}
                         className={`px-6 py-4 rounded-xl shadow-md font-medium text-lg duration-300 text-center focus:outline-none ${
                           darkMode
                             ? "bg-slate-800 border-4 border-transparent hover:border-cyan-700 hover:-translate-y-1 hover:shadow-xl hover:shadow-cyan-700/50 text-white"
@@ -187,7 +187,8 @@ function Quiz ({
                     <motion.button
                       onClick={calculateResult}
                       whileHover={{ scale: 1.05 }}
-                      whileTap={{ scale: 0.95 }}
+                      whileTap={{ scale: 0.9 }}
+                      key={`${currentQuestionIndex}-${index}`} 
                       className={`px-6 py-3 rounded-full font-semibold shadow-lg mt-4 duration-300 ${
                         darkMode
                           ? "bg-cyan-700 hover:bg-cyan-600 text-white"
