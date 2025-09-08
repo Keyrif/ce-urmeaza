edfa22f0ffa62accfbc3159afd205bc1e24e1ff8
KEYRIF <mihaikenj@gmail.com>
2025-09-08 03:20:54 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ function Footer({ darkMode }) {
       <a className="text-sm opacity-80" href="https://github.com/keyrif/ce-urmeaza">
         Pagina Proiect Github
       </a>
-      <p>v0.47xneumorphism</p>
+      <p>v0.47 lag</p>
     </footer>
   );
 }
